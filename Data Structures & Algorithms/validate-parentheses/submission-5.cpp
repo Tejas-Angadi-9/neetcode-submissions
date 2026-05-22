e05ba96929f29df6c510983b33c54370faf79fb6
Tejas Angadi <tejasangadi456@gmail.com>
2026-05-22 20:48:37 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        if (s.size() == 0) return true;
+
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[')
+                st.push(ch);
+            else {
+                if (st.empty()) return false;
+
+                char topChar = st.top();
+                st.pop();
+                if (ch == ')' && topChar != '(' || ch == '}' && topChar != '{' ||
+                    ch == ']' && topChar != '[') {
+                    return false;
+                }
+                
+            }
+        }
+        return st.empty();
+    }
+};
