54bb115987b06aa90f12176d95b6fcdb73a0f86d
Tejas Angadi <tejasangadi456@gmail.com>
2026-05-29 13:39:44 +0530

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    ListNode* findEndOfFirstHalf(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next && fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        ListNode* secondHalfHead = slow->next;
+        slow->next = nullptr;
+        return secondHalfHead;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    void reorderList(ListNode* head) {
+        ListNode* secondHalfHead = findEndOfFirstHalf(head);
+        ListNode* reversedHead = reverseList(secondHalfHead);
+
+        ListNode* firstPtr = head;
+        ListNode* secondPtr = reversedHead;
+
+        while (firstPtr && secondPtr) {
+            ListNode* firstNext = firstPtr->next;
+            ListNode* secondNext = secondPtr->next;
+
+            firstPtr->next = secondPtr;
+            secondPtr->next = firstNext;
+
+            firstPtr = firstNext;
+            secondPtr = secondNext;
+        }
+    }
+};
